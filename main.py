5bc39008b02137319a9ae4a50f482e1c2d6c347a
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-07 19:44:39 +0700

    Added handling for directory selection button click event

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
 from interface import Ui_Hidden_cipher
+
 
 class HiddenCipherApp(QMainWindow, Ui_Hidden_cipher):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Hidden Cipher")
-        self.setWindowIcon(QIcon("../../Загрузки/caesar-cipher.png"))
-        
-        # Добавьте здесь необходимую логику вашего приложения
-        
+        self.setWindowIcon(QIcon("/img/icon.png"))
+
+        self.path_selection_button.clicked.connect(
+            self.path_selection_button_clicked)
+
+    def path_selection_button_clicked(self):
+        options = QFileDialog.Options()
+
+        options |= QFileDialog.DontUseNativeDialog
+
+        selected_directory = QFileDialog.getExistingDirectory(
+            self, "Select Directory", options=options)
+
+        if selected_directory:
+            self.path_lineEdit.setText(selected_directory)
+
+
 def main():
     app = QApplication(sys.argv)
     window = HiddenCipherApp()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
