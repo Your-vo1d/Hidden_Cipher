d586d7b45417ccc1675cab5b714b17c5f9de9506
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-12 23:16:32 +0700

    Implemented button click handling for adding files for encoding and decoding.

--- event_handlers.py
+++ event_handlers.py
@@ -6,14 +6,24 @@ class EventHandlers:
         self.main_window = main_window
 
     def encode_file_clicked(self):
-        pass
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(
+            self.main_window, "Select File", "", "Text file (*.txt)", options=options)
+        if fileName:
+            self.main_window.path_encode_file.setText(fileName)
+
+    def decode_file_clicked(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(
+            self.main_window, "Select File", "", "Text file (*.txt)", options=options)
+        if fileName:
+            self.main_window.path_decode_file.setText(fileName)
 
     def about_button_clicked(self):
         pass
 
     def path_selection_button_clicked(self):
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
 
         selected_directory = QFileDialog.getExistingDirectory(
             self.main_window, "Select Directory", options=options)
